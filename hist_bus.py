f88106c1bc9ccfab08025cf86b162ea2c4221d5d
Ben Gamari <bgamari.foss@gmail.com>
2011-01-30 18:10:12 -0500

    hist_bus: Take input on stdin

--- hist_bus.py
+++ hist_bus.py
@@ -3,6 +3,7 @@
 from matplotlib import pyplot as pl
 import numpy as np
 from numpy import min, max, pi
+import sys
 
 route = '30'
 plot_stops = False
@@ -11,7 +12,7 @@ bin_dist = 10  # meters
 bin_width = bin_dist / Rearth * 180 / pi  # degrees
 
 dt = [('bus_n', '5a'), ('route', '5a'), ('time', 'u8'), ('lat', 'f'), ('lon', 'f')]
-d = np.genfromtxt('h', dtype=dt)
+d = np.genfromtxt(sys.stdin, dtype=dt)
 if route:
         d = d[d['route'] == route]
 
