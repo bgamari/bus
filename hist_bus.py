2cdc83c1affeab1386f2aca635ec16b8aea64098
Ben Gamari <bgamari.foss@gmail.com>
2011-01-30 18:15:56 -0500

    hist_bus: Remove filtering

--- hist_bus.py
+++ hist_bus.py
@@ -5,7 +5,6 @@ import numpy as np
 from numpy import min, max, pi
 import sys
 
-route = '30'
 plot_stops = False
 Rearth = 6400e3  # meters
 bin_dist = 10  # meters
@@ -13,8 +12,6 @@ bin_width = bin_dist / Rearth * 180 / pi  # degrees
 
 dt = [('bus_n', '5a'), ('route', '5a'), ('time', 'u8'), ('lat', 'f'), ('lon', 'f')]
 d = np.genfromtxt(sys.stdin, dtype=dt)
-if route:
-        d = d[d['route'] == route]
 
 bins = ((max(d['lat'])-min(d['lat'])) / bin_width,
         (max(d['lon'])-min(d['lon'])) / bin_width)
