5aab3d71b123a74c38cba4a4a7b9247d36d470b0
Ben Gamari <bgamari.foss@gmail.com>
2011-01-30 18:07:19 -0500

    hist_bus: Filter on route instead of bus_n

--- hist_bus.py
+++ hist_bus.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
+
 from matplotlib import pyplot as pl
 import numpy as np
 from numpy import min, max, pi
 
-bus_ns = ['3113']
+route = '30'
 plot_stops = False
 Rearth = 6400e3  # meters
 bin_dist = 10  # meters
@@ -10,11 +12,8 @@ bin_width = bin_dist / Rearth * 180 / pi  # degrees
 
 dt = [('bus_n', '5a'), ('route', '5a'), ('time', 'u8'), ('lat', 'f'), ('lon', 'f')]
 d = np.genfromtxt('h', dtype=dt)
-if bus_ns:
-        mask = np.zeros_like(d['time'])
-        for b in bus_ns:
-                mask = np.logical_or(mask, d['bus_n'] == b)
-        d = d[mask]
+if route:
+        d = d[d['route'] == route]
 
 bins = ((max(d['lat'])-min(d['lat'])) / bin_width,
         (max(d['lon'])-min(d['lon'])) / bin_width)
