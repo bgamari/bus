d2d293c6a4e25a0db4f6fb22cccb49383f0c190c
Ben Gamari <bgamari.foss@gmail.com>
2013-02-27 20:21:30 -0500

    bus_tracker: Update for infopoint

--- bus_tracker.py
+++ bus_tracker.py
@@ -1,33 +1,36 @@
 #!/usr/bin/python
 
-import json
+import xml.etree.ElementTree as ET
 from urllib2 import urlopen
 import datetime
+from time import time
 import os.path
 
 data_path = os.path.expanduser('~/bus/data')
-url = 'http://dieselnet.cs.umass.edu/bus_plotter'
-#routes = ['31', '45']
-routes = None # All routes
+url = 'http://uts.pvta.com:81/InfoPoint/map/GetVehicleXml.ashx?RouteId=%s'
+routes = ['30', '31', '38', '39', '45', '46']
 
+def fetch_route_vehicles(route):
+        f = urlopen(url % route)
+        d = ET.fromstring(f.read())
 
-f = urlopen(url)
-d = json.loads(f.read())
-#print json.dumps(d, sort_keys=True, indent=4)
+        vehicles = []
+        for b in d:
+                lat, lon = float(b.attrib['lat']), float(b.attrib['lng'])
+                bus_n = b.attrib['name']
+                vehicles.append((bus_n, (lat,lon)))
 
-for b in d['unitList']:
-        lat, lon = b['gps']['latitude'], b['gps']['longitude']
-        route = b['schedule']['route_id']
-        time = b['gps']['check_in_time']
-        bus_n = b['vehicle']['bus_number']
+        return vehicles
 
-        if time is None: continue
-        if routes is not None and route not in routes: continue
+for route in routes:
+        vehicles = fetch_route_vehicles(route)
+        t = time()
 
         p = os.path.join(data_path, 'route%s' % route)
         if not os.path.isdir(p):
                 os.makedirs(p)
 
         f = open(os.path.join(p, datetime.date.today().strftime('%Y%m%d')), 'a')
-        f.write('%5s\t%5s\t%20d\t%2.10f\t%2.10f\n' % (bus_n, route, time, lat, lon))
+        for (bus_n, (lat,lon)) in vehicles:
+                f.write('%5s\t%5s\t%20d\t%2.10f\t%2.10f\n' % (bus_n, route, t, lat, lon))
 
